4f19a8791d5dbeca0dd581c5d28e31a2cd7a3c0c
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-21 22:48:09 +0300

    Learning FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app=FastAPI()
 
@@ -9,10 +9,11 @@ app=FastAPI()
 
 students={
     1: {
-        "name": "john",
+        "name": "Joseph Mbote",
         "age": 24,
         "class": "Computer Science"
     }
+
 }
 
 @app.get("/")
@@ -20,5 +21,5 @@ async def index():
     return {"name": "First Data"}
 
 @app.get("/student/{student_id}")
-def get_student(student_id: int):
+def get_student(student_id: int=Path(None, description="The ID of the student you want to view")):
     return students[student_id]
