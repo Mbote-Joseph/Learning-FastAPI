3518d30b44ff65ca367f336e085b19d28fc6cbab
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-22 22:47:59 +0300

    Learning FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Path
+from typing import Union
+
 
 app=FastAPI()
 
@@ -42,3 +44,15 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
